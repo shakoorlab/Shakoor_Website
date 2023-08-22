b0f68e92bfa7a0509565a8c4ba8ec76f941c61da
Jake Stanton <jaccobstanton@gmail.com>
2023-08-22 14:00:48 -0500

    worked on teams page

--- src/components/pages/Team/Team.js
+++ src/components/pages/Team/Team.js
@@ -1,83 +1,168 @@
 import React from "react";
 import TeamItem from "./TeamItem";
 import "./Team.css";
+import TeamHero from "../../TeamHero/TeamHero";
 
 function Team() {
   return (
-    <div className="cards">
-      <h1>Our Team</h1>
-      <div className="cards__wrapper">
-        <ul className="cards__items">
-          <TeamItem
-            src="images/articles/article_11.JPG"
-            alt="Sorghum field"
-            text="Harnessing the Power of Climate Smart Crops"
-            label="Principal Investigator"
-            path="https://www.bizjournals.com/stlouis/news/2023/01/01/sorghum-harnessing-the-power-of-climate-smart-crop.html"
-          />
-          <TeamItem
-            src="images/articles/article_10.jpeg"
-            alt="Farmer in field"
-            text="Sorghum's Revival Goes Against the Grain"
-            label="September 30, 2022"
-            path="https://www.forbes.com/sites/daphneewingchow/2022/09/30/sorghums-revival-goes-against-the-grain/?sh=1fede1d66f1c"
-          />
-          <TeamItem
-            src="images/articles/article_8.jpeg"
-            alt="Photo of Sorghum"
-            text="DDPSC Collaborates with Sorghum Producers"
-            label="September 28, 2022"
-            path="https://www.eurekalert.org/news-releases/966324"
-          />
-        </ul>
-        <ul className="cards__items">
-          <TeamItem
-            src="images/articles/article_7.jpeg"
-            alt="Photo at the NSF"
-            text="Receiving the National Science Foundation Grant"
-            label="September 15, 2022"
-            path="https://www.slu.edu/news/2022/september/nsf-grant-edge-computing.php"
-          />
-          <TeamItem
-            src="images/articles/article_1.jpg"
-            alt="Image of PheNode in field "
-            text="Salk Researchers Launch Colaboration"
-            label="September 14, 2021"
-            path="https://www.salk.edu/news-release/salk-plant-researchers-launch-collaboration-to-breed-carbon-capturing-sorghum/"
-          />
-          <TeamItem
-            src="images/articles/article_6.jpeg"
-            alt="Image of Dr. Shakoor working with PheNode"
-            text="You Should Get to Know Dr. Shakoor"
-            label="August 5, 2021"
-            path="https://www.bizjournals.com/stlouis/inno/stories/profiles/2021/08/05/st-louis-character-2021-nadia-shakoor.html"
-          />
-        </ul>
-        <ul className="cards__items">
-          <TeamItem
-            src="images/articles/article_5.jpeg"
-            alt="Image of phenotyper"
-            text="ETHINKSTL: In This Episode for the Week"
-            label=" February 10, 2021"
-            path="https://entrepreneuriallythinking.libsyn.com/ethinkstl-ep213-season16-ep10-nadia-shakoor"
-          />
-          <TeamItem
-            src="images/articles/article_2.jpeg"
-            alt="Image of TerraReff project"
-            text="Farming and Drones? How do they work together?"
-            label="November 16, 2020"
-            path="https://www.stlmag.com/news/nadia-shakoor-field-dock-farming/"
-          />
-          <TeamItem
-            src="images/articles/article_3.jpeg"
-            alt="Image of PheNode in a field"
-            text="Phenotyping Station Ready for Field Testing"
-            label="November 4, 2016"
-            path="https://www.agupdate.com/crops/phenotyping-station-ready-for-field-testing/article_dd916df4-5ed8-5da9-a8cb-0b6696e4d9e1.html"
-          />
-        </ul>
+    <>
+      <TeamHero /> {/* Inserting the TeamHero component here */}
+      <div className="cards">
+        <h1>Our Team</h1>
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            <TeamItem
+              src="images/team_headshots/Nadia_Shakoor.jpeg"
+              alt="Nadia Shakoor"
+              text="Nadia Shakoor "
+              label="Principal Investigator"
+              path="https://www.bizjournals.com/stlouis/news/2023/01/01/sorghum-harnessing-the-power-of-climate-smart-crop.html"
+            />
+            <TeamItem
+              src="images/team_headshots/Jocelyn_Saxton.jpg"
+              alt="Jocelyn Saxton"
+              text="Jocelyn Saxton"
+              label="September 30, 2022"
+              path="https://www.forbes.com/sites/daphneewingchow/2022/09/30/sorghums-revival-goes-against-the-grain/?sh=1fede1d66f1c"
+            />
+            <TeamItem
+              src="images/team_headshots/Marie_De.png"
+              alt="Marie De Gracia Coquerel"
+              text="Marie De Gracia Coquerel"
+              label="September 28, 2022"
+              path="https://www.eurekalert.org/news-releases/966324"
+            />
+            <TeamItem
+              src="images/team_headshots/Nate_Eck.png"
+              alt="Nathaniel Eck"
+              text="Nathaniel Eck"
+              label="September 15, 2022"
+              path="https://www.slu.edu/news/2022/september/nsf-grant-edge-computing.php"
+            />
+          </ul>
+          <ul className="cards__items">
+            <TeamItem
+              src="images/team_headshots/Jake_Stanton.jpg"
+              alt="Jaccob Stanton"
+              text="Jaccob Stanton"
+              label="September 15, 2022"
+              path="https://www.slu.edu/news/2022/september/nsf-grant-edge-computing.php"
+            />
+            <TeamItem
+              src="images/team_headshots/Monica_Alsup.jpeg"
+              alt="Monica Alsup "
+              text="Monica Alsup"
+              label="September 14, 2021"
+              path="https://www.salk.edu/news-release/salk-plant-researchers-launch-collaboration-to-breed-carbon-capturing-sorghum/"
+            />
+            <TeamItem
+              src="images/team_headshots/Boubacar_Gano.png"
+              alt="Boubacar Gano"
+              text="Boubacar Gano"
+              label="August 5, 2021"
+              path="https://www.bizjournals.com/stlouis/inno/stories/profiles/2021/08/05/st-louis-character-2021-nadia-shakoor.html"
+            />
+            <TeamItem
+              src="images/team_headshots/Nurzaman_Ahmed.png"
+              alt="Nurzaman Ahmed"
+              text="Nurzaman Ahmed"
+              label="September 15, 2022"
+              path="https://www.slu.edu/news/2022/september/nsf-grant-edge-computing.php"
+            />
+          </ul>
+          <ul className="cards__items">
+            <TeamItem
+              src="images/team_headshots/Daniele_Azzaro.jpg"
+              alt="Daniele Azzaro"
+              text="Daniele Azzaro"
+              label=" February 10, 2021"
+              path="https://entrepreneuriallythinking.libsyn.com/ethinkstl-ep213-season16-ep10-nadia-shakoor"
+            />
+            <TeamItem
+              src="images/team_headshots/Rachel_Lange.png"
+              alt="Rachel Lange"
+              text="Rachel Lange"
+              label="November 16, 2020"
+              path="https://www.stlmag.com/news/nadia-shakoor-field-dock-farming/"
+            />
+            <TeamItem
+              src="images/team_headshots/Amy_Pfeifer.png"
+              alt="Amy Pfeifer"
+              text="Amy Pfeifer"
+              label="November 4, 2016"
+              path="https://www.agupdate.com/crops/phenotyping-station-ready-for-field-testing/article_dd916df4-5ed8-5da9-a8cb-0b6696e4d9e1.html"
+            />
+            <TeamItem
+              src="images/team_headshots/Brendan_Eddingfield.png"
+              alt="Brendan Eddingfield"
+              text="Brendan Eddingfield"
+              label="September 15, 2022"
+              path="https://www.slu.edu/news/2022/september/nsf-grant-edge-computing.php"
+            />
+          </ul>
+          <ul className="cards__items">
+            <TeamItem
+              src="images/team_headshots/Zander_Hayes.jpg"
+              alt="Zander Hayes"
+              text="Zander Hayes"
+              label=" February 10, 2021"
+              path="https://entrepreneuriallythinking.libsyn.com/ethinkstl-ep213-season16-ep10-nadia-shakoor"
+            />
+            <TeamItem
+              src="images/team_headshots/Audrey_Oldani.jpg"
+              alt="Audrey Oldani"
+              text="Audrey Oldani"
+              label="November 16, 2020"
+              path="https://www.stlmag.com/news/nadia-shakoor-field-dock-farming/"
+            />
+            <TeamItem
+              src="images/team_headshots/Ella_Marks.png"
+              alt="Ella Marks"
+              text="Ella Marks"
+              label="November 4, 2016"
+              path="https://www.agupdate.com/crops/phenotyping-station-ready-for-field-testing/article_dd916df4-5ed8-5da9-a8cb-0b6696e4d9e1.html"
+            />
+            <TeamItem
+              src="images/team_headshots/Colton_Burris.jpg"
+              alt="Colton Burris"
+              text="Colton Burris"
+              label="September 15, 2022"
+              path="https://www.slu.edu/news/2022/september/nsf-grant-edge-computing.php"
+            />
+          </ul>
+          <ul className="cards__items">
+            <TeamItem
+              src="images/team_headshots/Aaron_Fackler.jpg"
+              alt="Aaron Fackler"
+              text="Aaron Fackler"
+              label=" February 10, 2021"
+              path="https://entrepreneuriallythinking.libsyn.com/ethinkstl-ep213-season16-ep10-nadia-shakoor"
+            />
+            <TeamItem
+              src="images/team_headshots/"
+              alt="Samantha Aarons"
+              text="Samantha Aarons"
+              label="November 16, 2020"
+              path="https://www.stlmag.com/news/nadia-shakoor-field-dock-farming/"
+            />
+            <TeamItem
+              src="images/team_headshots/"
+              alt="Image of PheNode in a field"
+              text="Phenotyping Station Ready for Field Testing"
+              label="November 4, 2016"
+              path="https://www.agupdate.com/crops/phenotyping-station-ready-for-field-testing/article_dd916df4-5ed8-5da9-a8cb-0b6696e4d9e1.html"
+            />
+            <TeamItem
+              src="images/team_headshots/"
+              alt="Photo at the NSF"
+              text="Receiving the National Science Foundation Grant"
+              label="September 15, 2022"
+              path="https://www.slu.edu/news/2022/september/nsf-grant-edge-computing.php"
+            />
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
