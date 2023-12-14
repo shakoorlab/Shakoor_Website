dbe34db4204021440b8f6ce82deeca9c2e1e7fac
JaccobStanton <jstanton23@my.stlcc.edu>
2023-12-14 14:52:27 -0600

    Replaced old video source with new video that is smaller and has a better transition

--- src/components/pages/Hero/HeroSection.js
+++ src/components/pages/Hero/HeroSection.js
@@ -49,7 +49,7 @@ function HeroSection() {
   return (
     <div className="hero-container">
       {isLargerScreen ? (
-        <video src="/videos/background_video.mp4" autoPlay loop muted />
+        <video src="/videos/background_video.webm" autoPlay loop muted />
       ) : (
         <img
           src="/images/home/background_image.webp"
