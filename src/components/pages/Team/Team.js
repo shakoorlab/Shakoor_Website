b50f06ea98629bcb2fa6a6ccd40865a608199582
JaccobStanton <jstanton23@my.stlcc.edu>
2023-12-27 16:03:15 -0600

    Rearranged <ul> elements to fit everyone from lab on page

--- src/components/pages/Team/Team.js
+++ src/components/pages/Team/Team.js
@@ -30,14 +30,15 @@ function Team() {
               text="Marie De Gracia Coquerel"
               label="Research Associate"
             />
+          </ul>
+
+          <ul className="cards__items">
             <TeamItem
               src="images/team_headshots/Nate_Eck.webp"
               alt="Nathaniel Eck"
               text="Nathaniel Eck"
               label="Data Scientist 1"
             />
-          </ul>
-          <ul className="cards__items">
             <TeamItem
               src="images/team_headshots/Jake_Stanton.webp"
               alt="Jaccob Stanton"
@@ -50,6 +51,8 @@ function Team() {
               text="Monica Alsup"
               label="Administrative Assistant"
             />
+          </ul>
+          <ul className="cards__items">
             <TeamItem
               src="images/team_headshots/Boubacar_Gano.webp"
               alt="Boubacar Gano"
@@ -62,14 +65,15 @@ function Team() {
               text="Nurzaman Ahmed"
               label="Engineering Research Scientist"
             />
-          </ul>
-          <ul className="cards__items">
             <TeamItem
               src="images/team_headshots/Daniele_Azzaro.webp"
               alt="Daniele Azzaro"
               text="Daniele Azzaro"
               label="Research Associate"
             />
+          </ul>
+
+          <ul className="cards__items">
             <TeamItem
               src="images/team_headshots/Audrey_Oldani.webp"
               alt="Audrey Oldani"
@@ -108,11 +112,25 @@ function Team() {
               text="Lee Dobson"
               label="Laboratory Technician"
             />
+          </ul>
+          <ul className="cards__items">
+            <TeamItem
+              src="images/team_headshots/Danforth_Logo.webp"
+              alt="Paige Michalik"
+              text="Paige Michalik"
+              label="Laboratory Technician"
+            />
             <TeamItem
               src="images/team_headshots/Danforth_Logo.webp"
               alt="Keiko Farah"
               text="Keiko Farah"
               label="Laboratory Technician"
+            />
+            <TeamItem
+              src="images/team_headshots/Alice_Kujur.webp"
+              alt="Alice Kujur"
+              text="Alice Kujur"
+              label="Research Scientist"
             />
           </ul>
         </div>
